0a7de04b2f6635d593f014a1f4cf209d1e35a9c6
Hunter Fehr <hunter.fehr3935@yahoo.com>
2023-01-31 14:54:14 -0800

    added footer, header, navbase, and the ticket form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import Table from './components/Table/index';
+// import Table from './components/Table/index';
 import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Navigation from './Components/Navigation';
+import TicketForm from './Components/TicketForm';
 
 function App() {
   return (
    <>
-   <Table />
+   <Header />
+   <Navigation />
+   <TicketForm />
+   {/* <Table /> */}
+   <Footer />
    </>
   );
 }
