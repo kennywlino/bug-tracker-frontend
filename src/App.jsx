a677f8a615c12f1410e29f2515e4f90926a2bc0d
Trace Dugar <tracedugar89@gmail.com>
2023-01-31 16:35:32 -0800

    TeamTable , Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
+import { Route, Routes } from 'react-router-dom';
 import TicketTable from './components/TicketTable/index';
 import StatsCard from './components/Progress/index';
 import './App.css';
+import AuthenticationImage from '../src/components/Login';
+import TeamsTable from './components/TeamsTable';
 
 
 const App = () => {
   return (
-   <>
-   <StatsCard />
-   <TicketTable />
-   </>
+    <>
+      <Routes>
+        <Route
+          path='/login'
+          element={
+            <>
+              <AuthenticationImage />
+            </>
+          }
+        />
+        <Route
+          path='/dashboard'
+          element={
+            <>
+              <StatsCard />
+              <TicketTable />
+            </>
+          }
+        />
+        <Route
+          path='/teams'
+          element={
+            <>
+            <TeamsTable />
+            </>
+          }
+        />
+      </Routes>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import { MantineProvider } from '@mantine/core';
@@ -7,9 +8,11 @@ import { MantineProvider } from '@mantine/core';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-    <App />
-    </MantineProvider>
+    <BrowserRouter>
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <App />
+      </MantineProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
